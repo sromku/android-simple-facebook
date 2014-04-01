207238a332c018d4bfa5bcb1e50f47379f2f779c
Roman Kushnarenko <sromku@gmail.com>
2014-04-01 09:06:23 +0300

    Updated sample app to 1.4

--- Simple Facebook Sample/src/com/sromku/simple/fb/example/SampleApplication.java
+++ Simple Facebook Sample/src/com/sromku/simple/fb/example/SampleApplication.java
@@ -9,37 +9,36 @@ import com.sromku.simple.fb.SimpleFacebookConfiguration;
 import com.sromku.simple.fb.utils.Logger;
 
 public class SampleApplication extends Application {
-    private static final String APP_ID = "625994234086470";
-    private static final String APP_NAMESPACE = "sromkuapp";
+	private static final String APP_ID = "625994234086470";
+	private static final String APP_NAMESPACE = "sromkuapp";
 
-    @Override
-    public void onCreate() {
-	super.onCreate();
+	@Override
+	public void onCreate() {
+		super.onCreate();
 
-	// set log to true
-	Logger.DEBUG_WITH_STACKTRACE = true;
+		// set log to true
+		Logger.DEBUG_WITH_STACKTRACE = true;
 
-	// initialize facebook configuration
-	Permission[] permissions = new Permission[] { 
-		Permission.BASIC_INFO, 
-		Permission.USER_CHECKINS,
-		Permission.USER_EVENTS,
-		Permission.USER_GROUPS,
-		Permission.USER_LIKES,
-		Permission.USER_PHOTOS,
-		Permission.USER_VIDEOS,
-		Permission.FRIENDS_EVENTS,
-		Permission.PUBLISH_STREAM
-	};
+		// initialize facebook configuration
+		Permission[] permissions = new Permission[] { 
+				Permission.BASIC_INFO, 
+				Permission.USER_CHECKINS, 
+				Permission.USER_EVENTS, 
+				Permission.USER_GROUPS, 
+				Permission.USER_LIKES, 
+				Permission.USER_PHOTOS,
+				Permission.USER_VIDEOS, 
+				Permission.FRIENDS_EVENTS, 
+				Permission.PUBLISH_STREAM };
 
-	SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
-		.setAppId(APP_ID)
-		.setNamespace(APP_NAMESPACE)
-		.setPermissions(permissions)
-		.setDefaultAudience(SessionDefaultAudience.FRIENDS)
-		.setAskForAllPermissionsAtOnce(false)
-		.build();
+		SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
+			.setAppId(APP_ID)
+			.setNamespace(APP_NAMESPACE)
+			.setPermissions(permissions)
+			.setDefaultAudience(SessionDefaultAudience.FRIENDS)
+			.setAskForAllPermissionsAtOnce(false)
+			.build();
 
-	SimpleFacebook.setConfiguration(configuration);
-    }
+		SimpleFacebook.setConfiguration(configuration);
+	}
 }
