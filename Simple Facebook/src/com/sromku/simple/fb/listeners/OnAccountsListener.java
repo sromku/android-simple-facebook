bd0d8e1b1dbd628d3c6baec181e9cf42f0131533
Roman Kushnarenko <sromku@gmail.com>
2014-04-30 08:31:55 +0300

    Add `Account` entities

---
+++ Simple Facebook/src/com/sromku/simple/fb/listeners/OnAccountsListener.java
@@ -0,0 +1,14 @@
+package com.sromku.simple.fb.listeners;
+
+import java.util.List;
+
+import com.sromku.simple.fb.entities.Account;
+
+/**
+ * On accounts request listener
+ * 
+ * @author sromku
+ * 
+ */
+public abstract class OnAccountsListener extends OnActionListener<List<Account>> {
+}
