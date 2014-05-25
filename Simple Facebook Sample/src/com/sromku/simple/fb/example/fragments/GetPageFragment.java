38028fbc8b7708e65179c41ff29753109f1092b7
Roman Kushnarenko <sromku@gmail.com>
2014-05-25 09:48:00 +0300

    Added 'get page' example

--- Simple Facebook Sample/src/com/sromku/simple/fb/example/fragments/GetPageFragment.java
+++ Simple Facebook Sample/src/com/sromku/simple/fb/example/fragments/GetPageFragment.java
@@ -1,7 +1,78 @@
 package com.sromku.simple.fb.example.fragments;
 
-import android.support.v4.app.Fragment;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GetPageFragment extends Fragment{
+import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.entities.Page;
+import com.sromku.simple.fb.example.R;
+import com.sromku.simple.fb.example.utils.Utils;
+import com.sromku.simple.fb.listeners.OnPageListener;
 
+public class GetPageFragment extends BaseFragment{
+
+	private TextView mResult;
+	private Button mGetButton;
+	private TextView mMore;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActivity().setTitle("Get page");
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_get_music, container, false);
+		mResult = (TextView) view.findViewById(R.id.result);
+		mMore = (TextView) view.findViewById(R.id.load_more);
+		mMore.setPaintFlags(mMore.getPaint().getFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		mGetButton = (Button) view.findViewById(R.id.button);
+		disableLoadMore();
+		mGetButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				String pageId = "117713628271096";
+				SimpleFacebook.getInstance().getPage(pageId, new OnPageListener() {
+
+					@Override
+					public void onThinking() {
+						showDialog();
+					}
+
+					@Override
+					public void onException(Throwable throwable) {
+						hideDialog();
+						mResult.setText(throwable.getMessage());
+					}
+
+					@Override
+					public void onFail(String reason) {
+						hideDialog();
+						mResult.setText(reason);
+					}
+
+					@Override
+					public void onComplete(Page response) {
+						hideDialog();
+						String str = Utils.toHtml(response);
+						mResult.setText(Html.fromHtml(str));
+					}
+				});
+			}
+		});
+		return view;
+	}
+
+	private void disableLoadMore() {
+		mMore.setOnClickListener(null);
+		mMore.setVisibility(View.INVISIBLE);
+	}
 }
