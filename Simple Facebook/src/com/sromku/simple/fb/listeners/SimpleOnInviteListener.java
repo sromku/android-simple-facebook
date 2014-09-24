7d658b1d8bf92545dae7baa8631a1f3c9d080491
Andrew Chen <yongjhih@gmail.com>
2014-09-24 13:37:29 +0800

    Add SimpleOnInviteListener allows ignore implementations

---
+++ Simple Facebook/src/com/sromku/simple/fb/listeners/SimpleOnInviteListener.java
@@ -0,0 +1,21 @@
+package com.sromku.simple.fb.listeners;
+
+import java.util.List;
+
+public class SimpleOnInviteListener implements OnInviteListener {
+    @Override
+    public void onComplete(List<String> invitedFriends, String requestId) {
+    }
+
+    @Override
+    public void onCancel() {
+    }
+
+    @Override
+    public void onException(Throwable throwable) {
+    }
+
+    @Override
+    public void onFail(String reason) {
+    }
+}
