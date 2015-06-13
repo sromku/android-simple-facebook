4b96960ca52633fcc1956932b572b8205366b609
Roman Kushnarenko <sromku@gmail.com>
2015-06-13 09:51:44 +0300

    Revive invite functionality

--- library/src/main/java/com/sromku/simple/fb/actions/InviteAction.java
+++ library/src/main/java/com/sromku/simple/fb/actions/InviteAction.java
@@ -1,10 +1,11 @@
 package com.sromku.simple.fb.actions;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.os.Bundle;
-import android.text.TextUtils;
 
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.share.model.GameRequestContent;
+import com.facebook.share.widget.GameRequestDialog;
 import com.sromku.simple.fb.SessionManager;
 import com.sromku.simple.fb.listeners.OnInviteListener;
 import com.sromku.simple.fb.utils.Errors;
@@ -12,7 +13,6 @@ import com.sromku.simple.fb.utils.Errors.ErrorMsg;
 import com.sromku.simple.fb.utils.Logger;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class InviteAction extends AbstractAction {
 
@@ -54,20 +54,7 @@ public class InviteAction extends AbstractAction {
 	@Override
 	protected void executeImpl() {
 		if (sessionManager.isLogin()) {
-			Bundle params = new Bundle();
-			if (mMessage != null) {
-				params.putString(PARAM_MESSAGE, mMessage);
-			}
-			if (mData != null) {
-				params.putString(PARAM_DATA, mData);
-			}
-			if (mTo != null) {
-				params.putString(PARAM_TO, mTo);
-			}
-			else if (mSuggestions != null) {
-				params.putString(PARAM_SUGGESTIONS, TextUtils.join(",", mSuggestions));
-			}
-			openInviteDialog(sessionManager.getActivity(), params, mOnInviteListener);
+			openInviteDialog(sessionManager.getActivity(), mOnInviteListener);
 		}
 		else {
 			String reason = Errors.getError(ErrorMsg.LOGIN);
@@ -76,63 +63,53 @@ public class InviteAction extends AbstractAction {
 		}
 	}
 
-	private void openInviteDialog(Activity activity, Bundle params, final OnInviteListener onInviteListener) {
+	private void openInviteDialog(Activity activity, final OnInviteListener onInviteListener) {
 
-        // TODO - REVIVE !!
+        // build request
+        GameRequestContent.Builder builder = new GameRequestContent.Builder();
+        builder.setMessage(mMessage);
+        builder.setData(mData);
+        builder.setTo(mTo);
+        if (mSuggestions != null) {
+            ArrayList<String> arrayList = new ArrayList<String>();
+            for (String suggested : mSuggestions) {
+                arrayList.add(suggested);
+            }
+            builder.setSuggestions(arrayList);
+        }
 
-//		final Dialog dialog = new WebDialog.RequestsDialogBuilder(activity, Session.getActiveSession(), params).setOnCompleteListener(new WebDialog.OnCompleteListener() {
-//			@Override
-//			public void onComplete(Bundle values, FacebookException error) {
-//				if (error != null) {
-//					Logger.logError(InviteAction.class, "Failed to invite", error);
-//					if (error instanceof FacebookOperationCanceledException) {
-//						onInviteListener.onCancel();
-//					}
-//					else {
-//						if (onInviteListener != null) {
-//							onInviteListener.onException(error);
-//						}
-//					}
-//				}
-//				else {
-//					Object object = values.get("request");
-//					if (object == null) {
-//						onInviteListener.onCancel();
-//					}
-//					else {
-//						List<String> invitedFriends = fetchInvitedFriends(values);
-//						onInviteListener.onComplete(invitedFriends, object.toString());
-//					}
-//				}
-//			}
-//		}).build();
-//		Window dialogWindow = dialog.getWindow();
-//		dialogWindow.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//		dialog.show();
-	}
+        GameRequestContent gameRequestContent = builder.build();
 
-	/**
-	 * Fetch invited friends from response bundle
-	 * 
-	 * @param values
-	 * @return list of invited friends
-	 */
-	@SuppressLint("DefaultLocale")
-	private static List<String> fetchInvitedFriends(Bundle values) {
-		List<String> friends = new ArrayList<String>();
+        // set dialog
+        GameRequestDialog dialog = new GameRequestDialog(activity);
+        if (dialog.canShow(gameRequestContent)) {
+            dialog.registerCallback(sessionManager.getCallbackManager(), new FacebookCallback<GameRequestDialog.Result>() {
 
-		int size = values.size();
-		int numOfFriends = size - 1;
-		if (numOfFriends > 0) {
-			for (int i = 0; i < numOfFriends; i++) {
-				String key = String.format("to[%d]", i);
-				String friendId = values.getString(key);
-				if (friendId != null) {
-					friends.add(friendId);
-				}
-			}
-		}
-		return friends;
+                @Override
+                public void onSuccess(GameRequestDialog.Result result) {
+                    String requestId = result.getRequestId();
+                    if (mOnInviteListener != null) {
+                        mOnInviteListener.onComplete(result.getRequestRecipients(), requestId);
+                    }
+                }
+
+                @Override
+                public void onCancel() {
+                    if (mOnInviteListener != null) {
+                        mOnInviteListener.onFail("Canceled by user");
+                    }
+                }
+
+                @Override
+                public void onError(FacebookException e) {
+                    if (mOnInviteListener != null) {
+                        mOnInviteListener.onFail(e.getMessage());
+                    }
+                }
+            });
+            dialog.show(gameRequestContent);
+        }
+
 	}
 
 }
